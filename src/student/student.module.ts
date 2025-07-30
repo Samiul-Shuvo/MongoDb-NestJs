2ecbcbf13b50651b47f736d5364b9962a69f1b24
Samiul Shuvo <samiulshuvo.atom@gmail.com>
2025-07-31 01:37:29 +0600

    Create and Register Schema with Mongoose

---
+++ src/student/student.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Student, StudentSchema } from './student.schema';
+
+@Module({
+    imports:[
+        MongooseModule.forFeature([{name:Student.name, schema:StudentSchema}])
+    ]
+})
+export class StudentModule {}
